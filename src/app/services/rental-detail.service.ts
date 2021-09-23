30ad4ec5da2431b1a6d54ecdae0a15c4a858648f
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-23 15:47:33 +0300

    Added services

---
+++ src/app/services/rental-detail.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RentalDetailResponseModel } from '../models/rentalDetailResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RentalDetailService {
+
+  apiUrl= 'https://localhost:44311/api/Rentals/getrentaldetails'
+  constructor(private httpClient: HttpClient) { }
+
+  getRentalDetails(): Observable<RentalDetailResponseModel> {
+    return this.httpClient.get<RentalDetailResponseModel>(this.apiUrl);
+  }
+}
+
