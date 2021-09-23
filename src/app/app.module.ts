f2d6a54788fdfb3c49fe7ac13e73dacbc6a28efd
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-23 15:52:52 +0300

    Modified for newly added components and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './component/nav/nav.component';
+import { BrandComponent } from './component/brand/brand.component';
+import { CarComponent } from './component/car/car.component';
+import { ColorComponent } from './component/color/color.component';
+import { CustomerComponent } from './component/customer/customer.component';
+import { RentalComponent } from './component/rental/rental.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    BrandComponent,
+    CarComponent,
+    ColorComponent,
+    CustomerComponent,
+    RentalComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
