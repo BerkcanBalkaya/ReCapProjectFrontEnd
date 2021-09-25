0e2952378d8b0ded54623c1fed55c80415ae3d9c
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-25 18:11:23 +0300

    Modified for currentbrand selection methods

--- src/app/component/brand/brand.component.ts
+++ src/app/component/brand/brand.component.ts
@@ -1,25 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Brand } from 'src/app/models/brand';
 import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand',
   templateUrl: './brand.component.html',
-  styleUrls: ['./brand.component.scss']
+  styleUrls: ['./brand.component.scss'],
 })
 export class BrandComponent implements OnInit {
-
-  brands: Brand[] = [];
-  constructor(private brandService:BrandService) { }
+  brands: Brand[];
+  currentBrand: Brand;
+  constructor(private brandService: BrandService,private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.getBrands()
+    this.getBrands();
   }
 
-  getBrands(){
-    this.brandService.getBrands().subscribe(response=>{
-      this.brands = response.data
+  getBrands() {
+    this.brandService.getBrands().subscribe((response) => {
+      this.brands = response.data;
     });
   }
 
+  setCurrentBrand(brand: Brand) {
+    this.currentBrand = brand;
+  }
+
+  getCurrentBrandClass(brand: Brand) {
+    if (brand == this.currentBrand) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
+  }
 }
