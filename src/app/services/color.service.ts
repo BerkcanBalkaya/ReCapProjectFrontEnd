05d69d2ecf745df7d2795ef6a8710824531f0075
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-25 18:34:21 +0300

    Modified for backend method implementation

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -1,18 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ColorResponseModel } from '../models/colorResponseModel';
+import { Color } from '../models/color';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorService {
-  apiUrl= 'https://localhost:44311/api/Colors/getall'
+  apiUrl= 'https://localhost:44311/api/'
 
   constructor(private httpClient: HttpClient) { }
 
-  getColors():Observable<ColorResponseModel>{
-    return this.httpClient.get<ColorResponseModel>(this.apiUrl)
+  getColors():Observable<ListResponseModel<Color>>{
+    let newPath=this.apiUrl + 'Colors/getall';
+    return this.httpClient.get<ListResponseModel<Color>>(newPath)
+  }
+
+  getColorsById(colorId:number):Observable<ListResponseModel<Color>>{
+    let newPath=this.apiUrl + 'Colors/getbyid?id=' + colorId ;
+    return this.httpClient.get<ListResponseModel<Color>>(newPath);
   }
 
 }
