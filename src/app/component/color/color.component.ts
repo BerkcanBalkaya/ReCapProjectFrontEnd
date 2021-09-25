5df3b5587c7bb6d0215721271ee610f7d8b20167
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-25 18:24:34 +0300

    Modified for dynamic color list methods

--- src/app/component/color/color.component.ts
+++ src/app/component/color/color.component.ts
@@ -8,8 +8,8 @@ import { ColorService } from 'src/app/services/color.service';
   styleUrls: ['./color.component.scss']
 })
 export class ColorComponent implements OnInit {
-
-  colors:Color[]= [];
+  colors:Color[];
+  currentColor:Color;
   constructor(private colorService:ColorService) { }
 
   ngOnInit(): void {
@@ -20,6 +20,18 @@ export class ColorComponent implements OnInit {
     this.colorService.getColors().subscribe(response=>{
       this.colors = response.data
     });
+  }
+
+  setCurrentColor(color:Color){
+    this.currentColor=color;
+  }
+
+  getCurrentColorClass(color:Color){
+    if (color==this.currentColor) {
+      return 'list-group-item active';
+    }else{
+      return 'list-group-item';
+    }
   }
 }
 
