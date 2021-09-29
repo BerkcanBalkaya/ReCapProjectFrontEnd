edec9416ac761c2b884459bffc2cce91378315ec
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-29 14:17:13 +0300

    Modified by adding crud operations

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Car } from '../models/car';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +14,19 @@ export class CarService {
   apiUrl = 'https://localhost:44311/api/';
   constructor(private httpClient:HttpClient) { }
   
-  getCarsById(carId:number):Observable<ListResponseModel<Car>>{
+  add(car:Car):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"Cars/add",car)
+  }
+  update(car:Car):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"Cars/update",car)
+  }
+  delete(car:Car):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"Cars/delete",car)
+  }
+
+  getCarById(carId:number):Observable<SingleResponseModel<Car>>{
     let newPath = this.apiUrl+ 'Cars/getbyid?id='+carId;
-    return this.httpClient.get<ListResponseModel<Car>>(newPath);
+    return this.httpClient.get<SingleResponseModel<Car>>(newPath);
   }
 
 
