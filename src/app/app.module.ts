adf37db36eb0bb374845c1ff30b8a0224d2c3cfc
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-29 14:00:05 +0300

    Modified by adding multiple modules,imports and pr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +14,26 @@ import { CustomerComponent } from './component/customer/customer.component';
 import { RentalComponent } from './component/rental/rental.component';
 import { CarDetailsComponent } from './component/car-details/car-details.component';
 import { SanitizerService } from './services/sanitizer.service';
+import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { BrandFilterPipe } from './pipes/brand-filter.pipe';
+import { ColorFilterPipe } from './pipes/color-filter.pipe';
+import { CarDetailFilterPipe } from './pipes/car-detail-filter.pipe';
+import { PaymentComponent } from './component/payment/payment.component';
+import { CarAddComponent } from './component/car-add/car-add.component';
+import { BrandAddComponent } from './component/brand-add/brand-add.component';
+import { ColorAddComponent } from './component/color-add/color-add.component';
+import { ColorManagerComponent } from './component/color-manager/color-manager.component';
+import { ColorUpdateComponent } from './component/color-update/color-update.component';
+import { ColorDeleteComponent } from './component/color-delete/color-delete.component';
+import { CarUpdateComponent } from './component/car-update/car-update.component';
+import { BrandUpdateComponent } from './component/brand-update/brand-update.component';
+import { BrandManagerComponent } from './component/brand-manager/brand-manager.component';
+import { BrandDeleteComponent } from './component/brand-delete/brand-delete.component';
+import { CarDeleteComponent } from './component/car-delete/car-delete.component';
+import { CarManagerComponent } from './component/car-manager/car-manager.component';
+import { LoginComponent } from './component/login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 
 @NgModule({
@@ -24,13 +46,39 @@ import { SanitizerService } from './services/sanitizer.service';
     CustomerComponent,
     RentalComponent,
     CarDetailsComponent,
+    VatAddedPipe,
+    BrandFilterPipe,
+    ColorFilterPipe,
+    CarDetailFilterPipe,
+    PaymentComponent,
+    CarAddComponent,
+    BrandAddComponent,
+    ColorAddComponent,
+    ColorManagerComponent,
+    ColorUpdateComponent,
+    ColorDeleteComponent,
+    CarUpdateComponent,
+    BrandUpdateComponent,
+    BrandManagerComponent,
+    BrandDeleteComponent,
+    CarDeleteComponent,
+    CarManagerComponent,
+    LoginComponent,
   ],
   imports: [
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    }),
+    FormsModule,
     HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [SanitizerService],
+  providers: [SanitizerService,{
+    provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
