408e16825b2c1acfbc564f8f0e7a642b9a85768d
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-29 14:17:18 +0300

    Modified by adding crud operations

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Color } from '../models/color';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +14,26 @@ export class ColorService {
 
   constructor(private httpClient: HttpClient) { }
 
+  add(color:Color):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"Colors/add",color)
+  }
+
+  update(color:Color):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"Colors/update",color)
+  }
+
+  delete(color:Color):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"Colors/delete",color)
+  }
+
   getColors():Observable<ListResponseModel<Color>>{
     let newPath=this.apiUrl + 'Colors/getall';
     return this.httpClient.get<ListResponseModel<Color>>(newPath)
   }
 
-  getColorsById(colorId:number):Observable<ListResponseModel<Color>>{
+  getColorById(colorId:number):Observable<SingleResponseModel<Color>>{
     let newPath=this.apiUrl + 'Colors/getbyid?id=' + colorId ;
-    return this.httpClient.get<ListResponseModel<Color>>(newPath);
+    return this.httpClient.get<SingleResponseModel<Color>>(newPath);
   }
 
 }
